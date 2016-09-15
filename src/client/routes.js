9b32c5ba3cdcb9c324b0b43690e10c45cab2582b
dan-h-ch <chang.daniel@gmail.com>
2016-09-15 16:35:49 -0700

    (fix) removed partyparrot easter egg from code

--- src/client/routes.js
+++ src/client/routes.js
@@ -4,7 +4,6 @@ import CreateEvent from './components/CreateEvent';
 import EventDetails from './components/EventDetails';
 import UserDetails from './components/UserDetails';
 import Index from './components/Index';
-import PartyParrot from './components/PartyParrot';
 import { Route, IndexRoute } from 'react-router';
 import { LogoutRoute, AuthenticatedRoute } from 'react-stormpath';
 
@@ -16,7 +15,6 @@ export default (
       <Route path ="/create" component={CreateEvent} />
       <Route path ="/eventDetails" component={EventDetails} />
       <Route path ="/profile" component={UserDetails} />
-      <Route path ="/partyMode" component={PartyParrot} />
     </AuthenticatedRoute>
   </Route>
 );
