6cb7d109f5149043c34c26a50d9acec35c584fb1
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-18 16:01:04 -0500

    fixed event listing on profile page

--- src/client/components/userEvent.js
+++ src/client/components/userEvent.js
@@ -3,23 +3,24 @@ import React from 'react';
 export default class Event extends React.Component {
 
    componentDidMount() {
-    $('.card-txt').append(this.props.event.eventbrite.description.html)
+    // $('.card-txt').append(this.props.event.eventbrite.description.html)
   }
 
   render(){
     return (
       <div>
         <hr />
-          <div className="row">
-            <div className="col-md-12">
-              <img src={this.props.event.eventbrite.logo ? this.props.event.eventbrite.logo.url : "http://130.211.52.161/tradeo-content/themes/nucleare-pro/images/no-image-box.png"}  alt="" />
-            </div>
-
-            <div className="col-md-12" style={{"margin-top":"20px"}}>
-              <p className="card-txt"></p>
-            </div>
+        <div className="row">
+          <div className="col-md-12">
+            <img src={this.props.event.eventbrite.logo ? this.props.event.eventbrite.logo.url : "http://130.211.52.161/tradeo-content/themes/nucleare-pro/images/no-image-box.png"}  alt="" />
           </div>
-          <hr />
-        </div>)
+
+          <div className="col-md-12" style={{"margin-top":"20px"}}>
+            <p className="card-txt" dangerouslySetInnerHTML={{__html: this.props.event.eventbrite.description.html}}></p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
   }
 }
